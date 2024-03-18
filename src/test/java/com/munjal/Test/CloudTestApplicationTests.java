71640c6dcf445912b20cda93f800c7c6c36558f3
Aashray Munjal <aashraymunjal@gmail.com>
2024-03-18 16:47:26 -0400

    adding code

---
+++ src/test/java/com/munjal/Test/CloudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munjal.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
